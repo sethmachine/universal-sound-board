5583c1969ca0baf3e7c08ff6518b350934dd92eb
Seth Dworman <sdworman@hubspot.com>
2021-12-20 09:51:40 -0500

    fixes #10

--- src/main/java/io/sethmachine/universalsoundboard/core/model/concurrent/source/AudioFileStreamIF.java
+++ src/main/java/io/sethmachine/universalsoundboard/core/model/concurrent/source/AudioFileStreamIF.java
@@ -2,6 +2,7 @@ package io.sethmachine.universalsoundboard.core.model.concurrent.source;
 
 import com.hubspot.immutables.style.HubSpotStyle;
 import javax.sound.sampled.AudioInputStream;
+
 import org.immutables.value.Value.Immutable;
 
 @Immutable
